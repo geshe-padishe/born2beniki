d2aadd4951b8a6dab8b68a7da9d45b1efc9c3881
root <nikotikcho17@gmail.com>
2021-08-02 15:13:57 -0400

    amine shell

--- monitoring.sh
+++ monitoring.sh
@@ -5,6 +5,13 @@ free --mega | grep 'Mem' | awk '{printf("#Memory Usage: %s/%sMB (%.2f%%)\n", $3,
 df --total | grep 'total' | awk '{printf("#Disk Usage: %.f/%.fGb (%s)\n", $3 / 1000, $2 / 1000000, $5)}'
 top -bn1 | grep "Cpu(s)" | awk '{printf("#CPU load: %.1s%%\n", 100 - $8)}'
 who -bs | cut -b 23-38 | awk '{print("#Last boot: "$0)}'
+lines=`sudo lsblk | grep lvm | wc -l`;
+if [ $lines -gt 0 ]
+then
+	echo "LVM use: yes\n"
+else
+	echo "LVM use: no\n"
+fi
 ss -st | grep TCP: | grep -o '[0-9]' | sed '1d' | head -1 | awk '{print("#Connexions TCP : "$0" ESTABLISHED")}'
 who | wc -l | awk '{print("#User log: "$0)}'
 (hostname -I | awk '{print("#Netowrk: IP "$0)}' | tr '\n' ' ' | sed 's/.$//';cat /sys/class/net/enp0s3/address | awk '{print("("$0")")}')
